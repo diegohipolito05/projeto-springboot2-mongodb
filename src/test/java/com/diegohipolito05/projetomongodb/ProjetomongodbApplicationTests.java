9771b18fe29961206811c51e6af6f2a1bcfad058
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-16 15:44:10 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/diegohipolito05/projetomongodb/ProjetomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegohipolito05.projetomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
