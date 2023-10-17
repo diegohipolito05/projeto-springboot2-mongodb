c33a7dcf059266f1ef3267f886ed4ba41237c1cd
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-17 17:34:05 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/diegohipolito05/projetomongodb/config/Instatiation.java
+++ src/main/java/com/diegohipolito05/projetomongodb/config/Instatiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.diegohipolito05.projetomongodb.domain.Post;
 import com.diegohipolito05.projetomongodb.domain.User;
+import com.diegohipolito05.projetomongodb.dto.AuthorDTO;
 import com.diegohipolito05.projetomongodb.repository.PostRepository;
 import com.diegohipolito05.projetomongodb.repository.UserRepository;
 
@@ -31,10 +32,11 @@ public class Instatiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, LocalDate.parse("2018-03-21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", maria);
-		Post p2 = new Post(null, LocalDate.parse("2018-03-23"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post p1 = new Post(null, LocalDate.parse("2018-03-21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post p2 = new Post(null, LocalDate.parse("2018-03-23"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 	}
