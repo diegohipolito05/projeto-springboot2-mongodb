b7c7a4a1d81b476e5b76e9de9afd937998056c66
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-17 14:21:20 -0300

    Obtendo um usuário por id

--- src/main/java/com/diegohipolito05/projetomongodb/services/UserService.java
+++ src/main/java/com/diegohipolito05/projetomongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.diegohipolito05.projetomongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegohipolito05.projetomongodb.domain.User;
 import com.diegohipolito05.projetomongodb.repository.UserRepository;
+import com.diegohipolito05.projetomongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+	Optional<User> obj = repository.findById(id);
+	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/diegohipolito05/projetomongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.diegohipolito05.projetomongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
