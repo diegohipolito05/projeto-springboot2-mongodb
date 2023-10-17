b7fb37ff24090ca82425381a1354232091babfc7
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-17 11:17:06 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/diegohipolito05/projetomongodb/resources/UserResource.java
+++ src/main/java/com/diegohipolito05/projetomongodb/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diegohipolito05.projetomongodb.domain.User;
+import com.diegohipolito05.projetomongodb.dto.UserDTO;
 import com.diegohipolito05.projetomongodb.services.UserService;
 
 @RestController
@@ -19,9 +20,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
