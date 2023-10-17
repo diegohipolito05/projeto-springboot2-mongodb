7ab1235b9d08b6447be26904588beb30dacd4247
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-17 19:19:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/diegohipolito05/projetomongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.diegohipolito05.projetomongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegohipolito05.projetomongodb.domain.Post;
+import com.diegohipolito05.projetomongodb.repository.PostRepository;
+import com.diegohipolito05.projetomongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
