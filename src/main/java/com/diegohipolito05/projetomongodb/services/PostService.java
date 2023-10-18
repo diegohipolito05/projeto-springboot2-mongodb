0ee81928a9c5bccfe8d275b54dfb434b6dc42e92
Diego Hipolito <diegohipolito05@hotmail.com>
2023-10-18 16:39:08 -0300

    Consulta simples com query methods

--- src/main/java/com/diegohipolito05/projetomongodb/services/PostService.java
+++ src/main/java/com/diegohipolito05/projetomongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.diegohipolito05.projetomongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,11 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String title){
+		List<Post> list = postRepository.findByTitleContainingIgnoreCase(title);
+		return list;
 	}
 	
 }
